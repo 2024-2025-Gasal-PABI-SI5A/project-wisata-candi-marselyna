630e46c8ac3975cdd7ac2c0ebbfb72a6dd2cc563
marselyna <marselynafitrimawarni@gmail.com>
2024-11-16 10:58:49 +0700

    membuat projek wisata candi

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/signin_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,21 +14,29 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wisata Candi',
+      title: 'wisata candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.blueAccent),
+            titleTextStyle: TextStyle(
+              color: Color.fromARGB(255, 56, 112, 185),
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: Colors.blueAccent).copyWith(
+            primary: Colors.blueAccent,
+            surface: Colors.blueGrey[50],
+          ),
+          useMaterial3: true),
+
+      home: DetailScreen(
+        candi: candiList[0],
       ),
-      home: DetailScreen(),
+
+      // home: ProfileScreen(),
+      // home: SignInScreen(),
     );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
